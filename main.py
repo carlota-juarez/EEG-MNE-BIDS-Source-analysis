3d9ece3049a118266c6c0615d412f87f0b13b06f
Carlota <carlotajuarezalonso@gmail.com>
2026-07-09 10:44:21 +0200

    App BIDS

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ with open(file_name, 'w') as f:
 
     if needs_recon_all:
 
-        if not t1_path is None or not t1_path.exists():
+        if t1_path is None or not t1_path.exists():
             raise FileNotFoundError("A T1w es needed to execute recon-all or set 'use_template_mri' to skip it")
         # Copy the t1w file to the BIDS directory 
         anat_dir = deriv_root/f'sub-{subject}'/'anat'
